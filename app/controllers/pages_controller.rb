857864756d91be315100531f2bd0c54c4cd3b309
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-04-12 11:40:12 -0700

    redirect user to login page if not signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:profile]
 
   def index
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    end
+
     @users = User.all
     @posts = Post.all
   end
