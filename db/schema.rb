45b6689b55632212bc16c5703e4cbfeb2aa49eba
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-04-18 06:35:50 -0700

    generate a Comment model and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_042112) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_133440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.bigint "from_friend_id", null: false
@@ -47,6 +57,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_042112) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "friendships", "users", column: "from_friend_id"
   add_foreign_key "friendships", "users", column: "to_friend_id"
   add_foreign_key "posts", "users"
